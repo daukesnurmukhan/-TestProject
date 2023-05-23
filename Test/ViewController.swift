42e0b5bc2097d6c1f652f510ea1b513d695cd326
daukesnurmukhan <daukesnurmukhan@gmail.com>
2023-05-23 12:22:13 +0500

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Нұрмұхан Дәукес on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
